47512a9ceac87a2a15f2ff519bd996d68e9cb0ab
pjongy <hi.pjongy@gmail.com>
2020-11-22 22:17:20 +0900

    Apply example codes

---
+++ example/main.py
@@ -0,0 +1,36 @@
+import asyncio
+import logging
+
+import aiomysql
+
+from dispatcher.tasks import TasksDispatcher
+from repository.tasks import TaskRepository
+
+loop = asyncio.get_event_loop()
+
+
+async def run():
+    FORMAT = "[%(filename)s:%(lineno)s - %(funcName)s ] %(message)s"
+    logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+    pool = await aiomysql.create_pool(
+        host='127.0.0.1',
+        port=3306,
+        user='root',
+        db='test',
+        loop=loop,
+        autocommit=False,
+    )
+    dispatcher = TasksDispatcher(repository=TaskRepository(pool=pool))
+    await dispatcher.apply_tasks(
+        [
+            {'a': 1},
+            {'b': 1}
+        ]
+    )
+    tasks = await dispatcher.fetch_scheduled_tasks(0, 10)
+    pending_tasks = await dispatcher.fetch_pending_tasks(0, 10)
+    logging.info(tasks)
+    logging.info(pending_tasks)
+
+
+loop.run_until_complete(run())
