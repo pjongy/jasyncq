2ed6d274aad52a697a5e7041266a2712bbf5f3ad
pjongy <hi.pjongy@gmail.com>
2020-11-22 22:11:58 +0900

    Implement queue task model

---
+++ model/task.py
@@ -0,0 +1,28 @@
+import enum
+import json
+from dataclasses import dataclass
+from uuid import UUID
+
+import deserialize
+
+
+@dataclass
+class TaskStatus(enum.IntEnum):
+    DEFERRED = 1
+    QUEUED = 2
+    WORK_IN_PROGRESS = 3
+    COMPLETED = 4
+
+
+@dataclass
+@deserialize.parser('uuid', UUID)
+@deserialize.parser('status', TaskStatus)
+@deserialize.parser('is_urgent', bool)
+@deserialize.parser('task', json.loads)
+class Task:
+    uuid: UUID
+    status: TaskStatus
+    progressed_at: int  # epoch timestamp
+    scheduled_at: int  # epoch timestamp
+    is_urgent: bool
+    task: dict
