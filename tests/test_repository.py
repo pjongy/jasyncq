cbf4543060201c7dbf373e41bcccce7140190dca
pjongy <hi.pjongy@gmail.com>
2021-05-01 18:59:00 +0900

    Add test for insert_tasks in repository

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,8 +1,10 @@
-from typing import Tuple, Any
+import json
+from typing import Tuple, Any, Dict
 
 import aiomysql
 import pytest
 from aiomysql import Pool, Connection, Cursor
+from jasyncq.repository.model.task import TaskRowIn, TaskRow, TaskStatus
 
 from jasyncq.repository.tasks import TaskRepository
 
@@ -35,3 +37,51 @@ async def test_initialize():
     rows = await _query(pool, 'SHOW TABLES')
     tables = [row[0] for row in rows]
     assert tables.index(f'jasyncq_{test_topic_name}') > 0
+
+
+@pytest.mark.asyncio
+async def test_insert_tasks():
+    pool: Pool = await aiomysql.create_pool(
+        host='127.0.0.1',
+        port=3306,
+        user='root',
+        db='test',
+        autocommit=False,
+    )
+    test_topic_name = random_string_lower()
+    repository = TaskRepository(pool=pool, topic_name=test_topic_name)
+    await repository.initialize()
+
+    test_tasks = [
+        TaskRowIn(task={'id': 1}, queue_name=random_string_lower()),
+        TaskRowIn(task={'id': 2}, queue_name=random_string_lower()),
+    ]
+    inserted_tasks = await repository.insert_tasks(test_tasks)
+    inserted_tasks_by_uuid: Dict[str, TaskRow] = {}
+    for inserted_task in inserted_tasks:
+        inserted_tasks_by_uuid[inserted_task.uuid] = inserted_task
+
+    columns = ['uuid', 'status', 'progressed_at', 'scheduled_at',
+               'is_urgent', 'task', 'queue_name', 'depend_on']
+    fetching_query = f'SELECT {",".join(columns)} FROM jasyncq_{test_topic_name}'
+    task_rows = await _query(pool, fetching_query)
+
+    for task_row in task_rows:
+        uuid = task_row[0]
+        status = task_row[1]
+        progressed_at = task_row[2]
+        scheduled_at = task_row[3]
+        is_urgent = task_row[4]
+        task = task_row[5]
+        queue_name = task_row[6]
+        depend_on = task_row[7]
+
+        assert uuid in inserted_tasks_by_uuid
+        assert inserted_tasks_by_uuid[uuid].status == status
+        assert inserted_tasks_by_uuid[uuid].status == TaskStatus.QUEUED
+        assert inserted_tasks_by_uuid[uuid].progressed_at == progressed_at
+        assert inserted_tasks_by_uuid[uuid].scheduled_at == scheduled_at
+        assert inserted_tasks_by_uuid[uuid].is_urgent == is_urgent
+        assert inserted_tasks_by_uuid[uuid].task == json.loads(task)
+        assert inserted_tasks_by_uuid[uuid].queue_name == queue_name
+        assert inserted_tasks_by_uuid[uuid].depend_on == depend_on
