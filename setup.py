11058c6c267aff83c8cc7719dceda6560304010a
pjongy <hi.pjongy@gmail.com>
2021-05-07 21:12:48 +0900

    Release 1.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='jasyncq',
-    version='1.1.1',
+    version='1.1.2',
     description='High reliability asynchronous queue using mysql(lock)',
     long_description=long_description,
     long_description_content_type="text/markdown",
